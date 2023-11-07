00be97e8243a8dc9f40117281e92a7ecbfdfd856
Indra_Bagus_GolB_mobile <indrabusiness00@gmail.com>
2023-11-07 10:19:45 +0700

    y

--- SensorSRFHC04.py
+++ SensorSRFHC04.py
@@ -8,40 +8,59 @@ trigger = 26
 echo = 16
 
 # Definisikan pin untuk tiga lampu
-lamp_pin_1 = Ganti Dek
-lamp_pin_2 = Ini Lagi
-lamp_pin_3 = ini Juga
+lamp_pin = 4
 
 # Inisialisasi GPIO untuk masing-masing lampu
 GPIO.setup(trigger, GPIO.OUT)
 GPIO.setup(echo, GPIO.IN)
-GPIO.setup(lamp_pin_1, GPIO.OUT)
-GPIO.setup(lamp_pin_2, GPIO.OUT)
-GPIO.setup(lamp_pin_3, GPIO.OUT)
+GPIO.setup(lamp_pin, GPIO.OUT)
+
 
 GPIO.output(trigger, GPIO.LOW)
 
+
 def get_range():
-    # Fungsi untuk mengukur jarak, seperti sebelumnya
+    GPIO.output(trigger, True)
+    time.sleep(0.00001)
+    GPIO.output(trigger, False)
+    timeout_counter = int(time.time())
+    start = time.time()
+
+    while GPIO.input(echo) == 0 and (int(time.time()) - timeout_counter) < 3:
+        start = time.time()
+        timeout_counter = int(time.time())
+
+    stop = time.time()
+
+    while GPIO.input(echo) == 1 and (int(time.time()) - timeout_counter) < 3:
+        stop = time.time()
+
+    elapsed = stop - start
+    distance = elapsed * 34320
+    distance = distance / 2
+    return distance
+
 
 try:
     while True:
         jarak = get_range()
-        print('Jarak terukur: %.2f Cm' % jarak)
+        print('Jarak terukur adalah: %.2f Cm' % jarak)
 
         if jarak < 8:
-            # Nyalakan semua lampu jika jarak kurang dari 8 cm
-            GPIO.output(lamp_pin_1, GPIO.HIGH)
-            GPIO.output(lamp_pin_2, GPIO.HIGH)
-            GPIO.output(lamp_pin_3, GPIO.HIGH)
+            for i in range(3):
+                GPIO.output(lamp_pin, GPIO.HIGH)
+                time.sleep(0.5)
+                GPIO.output(lamp_pin, GPIO.LOW)
+                time.sleep(0.5)
+        elif 8 <= jarak <= 20:
+            for i in range(5):
+                GPIO.output(lamp_pin, GPIO.HIGH)
+                time.sleep(0.2)
+                GPIO.output(lamp_pin, GPIO.LOW)
+                time.sleep(0.2)
         else:
-            # Matikan semua lampu jika jarak lebih dari atau sama dengan 8 cm
-            GPIO.output(lamp_pin_1, GPIO.LOW)
-            GPIO.output(lamp_pin_2, GPIO.LOW)
-            GPIO.output(lamp_pin_3, GPIO.LOW)
-
+            GPIO.output(lamp_pin, GPIO.LOW)
         time.sleep(1)
-
 except KeyboardInterrupt:
     pass
 finally:
