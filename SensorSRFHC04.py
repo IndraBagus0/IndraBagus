49b3fbf52a88dc00e97228e8da4cf97c19d3565b
Indra_Bagus_GolB_mobile <indrabusiness00@gmail.com>
2023-11-07 09:42:45 +0700

    tes

--- SensorSRFHC04.py
+++ SensorSRFHC04.py
@@ -1,58 +1,53 @@
 import RPi.GPIO as GPIO
 import time
 
-# Konfigurasi mode pin BCM dan menghilangkan peringatan GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-# Definisikan pin trigger dan echo
 trigger = 26
 echo = 16
+lamp_pin = 4  # Ganti dengan pin yang sesuai untuk lampu
 
-# Konfigurasi pin sebagai output (trigger) dan input (echo)
 GPIO.setup(trigger, GPIO.OUT)
 GPIO.setup(echo, GPIO.IN)
+GPIO.setup(lamp_pin, GPIO.OUT)
 
-# Set pin trigger awalnya ke LOW
 GPIO.output(trigger, GPIO.LOW)
 
-# Fungsi untuk mengukur jarak
-
-
 def get_range():
-    # Aktifkan trigger selama 10 mikrodetik
     GPIO.output(trigger, True)
     time.sleep(0.00001)
     GPIO.output(trigger, False)
-
-    # Inisialisasi waktu awal dan timeout counter
     timeout_counter = int(time.time())
     start = time.time()
 
-    # Tunggu echo menjadi HIGH (1) atau timeout maksimum 3 detik
     while GPIO.input(echo) == 0 and (int(time.time()) - timeout_counter) < 3:
         start = time.time()
+        timeout_counter = int(time.time())
 
-    # Reset timeout counter
-    timeout_counter = int(time.time())
     stop = time.time()
 
-    # Tunggu echo menjadi LOW (0) atau timeout maksimum 3 detik
     while GPIO.input(echo) == 1 and (int(time.time()) - timeout_counter) < 3:
         stop = time.time()
 
-    # Hitung waktu yang diperlukan untuk pulsa ultrasonik kembali
     elapsed = stop - start
-
-    # Hitung jarak dalam centimeter menggunakan kecepatan suara
     distance = elapsed * 34320
     distance = distance / 2
-
     return distance
 
+try:
+    while True:
+        jarak = get_range()
+        print('Jarak terukur: %.2f Cm' % jarak)
 
-# Loop tak terbatas untuk mengukur jarak terus-menerus
-while True:
-    jarak = get_range()
-    print('Jarak terukur: %.2f Cm' % jarak)
-    time.sleep(1)
+        if jarak < 8:
+            GPIO.output(lamp_pin, GPIO.HIGH)  # Nyalakan lampu
+        else:
+            GPIO.output(lamp_pin, GPIO.LOW)  # Matikan lampu
+
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
